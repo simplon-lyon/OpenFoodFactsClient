beef1d07117188aac0ebd14c3fee0f9d0193c990
matmorel <pub.imagiin@gmail.com>
2018-06-11 11:15:02 +0200

    add list-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ListPageComponent } from './list-page/list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListPageComponent
   ],
   imports: [
     BrowserModule
